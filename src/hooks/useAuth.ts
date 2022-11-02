d57b840724db2e8ddceb03b08c6f2e6bc7a03a94
youliangdao <youliangdaojing5@gmail.com>
2022-11-03 08:33:41 +0900

    💄style: ログイン時のメッセージ表示機能を実装する

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,23 +4,37 @@ import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
   const [loading, setLoading] = useState(false);
+  const { showMessage } = useMessage();
+
   const login = useCallback((id: string) => {
     setLoading(true);
     axios
       .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => {
         if (res.data) {
+          showMessage({
+            title: "ログインに成功しました",
+            status: "success",
+          });
           history.push("/home");
         } else {
+          showMessage({
+            title: "ユーザーが見つかりません",
+            status: "warning",
+          });
           alert("ユーザーが見つかりません");
         }
       })
       .catch(() => {
-        alert("ログインできません");
+        showMessage({
+          title: "ログインできません",
+          status: "error",
+        });
       })
       .finally(() => {
         setLoading(false);
