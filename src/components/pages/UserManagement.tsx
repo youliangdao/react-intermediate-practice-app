ab40b5c37e752bb16335fe56861105cef2ea5232
youliangdao <youliangdaojing5@gmail.com>
2022-11-03 09:58:49 +0900

    ✨feat: ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,38 @@
-import React, { FC, memo } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import React, { FC, memo, useEffect } from "react";
+
+import { useAllUsers } from "../../hooks/useAllUsers";
+import UserCard from "../organisms/user/UserCard";
 
 // eslint-disable-next-line react/display-name
 const UserManagement: FC = memo(() => {
-  return <div>UserManagement</div>;
+  const { fetchUsers, loading, users } = useAllUsers();
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
 export default UserManagement;
